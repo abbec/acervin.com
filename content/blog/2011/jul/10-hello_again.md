3a573216e9149a4fc53dc506a278d7a1e5fdd5f2
Albert Cervin <albert@nat0.se>
2011-07-10 15:21:26 +0200

    Blogging :)

---
+++ content/blog/2011/jul/10-hello_again.md
@@ -0,0 +1,10 @@
+---
+title: Hello World (again)
+kind: article
+created_at: 2011-07-10
+---
+
+Hello hello.
+-------------
+
+Sune.
--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,2 +1,4 @@
 include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Text
 include Portfolio
